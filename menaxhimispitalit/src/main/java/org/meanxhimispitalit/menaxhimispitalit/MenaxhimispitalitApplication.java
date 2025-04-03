7cc7c2e353220ab5de28c9ce10a490babb10630d
argjend780 <am67266@ubt-uni.net>
2025-04-03 22:56:00 +0200

    Spring initial

---
+++ menaxhimispitalit/src/main/java/org/meanxhimispitalit/menaxhimispitalit/MenaxhimispitalitApplication.java
@@ -0,0 +1,13 @@
+package org.meanxhimispitalit.menaxhimispitalit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenaxhimispitalitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenaxhimispitalitApplication.class, args);
+	}
+
+}
