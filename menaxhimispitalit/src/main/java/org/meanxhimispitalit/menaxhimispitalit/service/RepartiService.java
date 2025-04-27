d8a9d76ee2776530342fd5e7ce52b19aecd705b2
argjend780 <am67266@ubt-uni.net>
2025-04-27 20:43:03 +0200

    Feat: Crete entity Spitali,Reparti Backend

---
+++ menaxhimispitalit/src/main/java/org/meanxhimispitalit/menaxhimispitalit/service/RepartiService.java
@@ -0,0 +1,78 @@
+package org.meanxhimispitalit.menaxhimispitalit.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+import org.meanxhimispitalit.menaxhimispitalit.Entity.Reparti;
+import org.meanxhimispitalit.menaxhimispitalit.Entity.Spitali;
+import org.meanxhimispitalit.menaxhimispitalit.repository.RepartiRepository;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+
+public class RepartiService {
+	
+
+	    private final RepartiRepository repartiRepository;
+	    private final SpitaliService spitaliservice;
+
+        public RepartiService(RepartiRepository repartiRepository, SpitaliService spitaliservice) {
+            this.repartiRepository = repartiRepository;
+            this.spitaliservice = spitaliservice;
+        }
+
+	    public Reparti createNewReparti(Reparti newTask, Long departmentid, Long empoyeeid) {
+
+			Spitali employee= spitaliservice.getSpitali(departmentid, empoyeeid);
+
+			newTask.setSpitali(employee);
+
+			return repartiRepository.save(newTask);
+
+		}
+
+	 
+
+	    public List<Reparti> getTaskList(Long departamentiID, Long employeeID) {
+			return spitaliservice.getSpitali(departamentiID, employeeID).getRepartet();
+	    }
+
+	    public ResponseEntity<?> deleteTask(Long departmentId, Long employeeId, Long taskId) {
+	    	spitaliservice.getSpitali(departmentId, employeeId);
+	        Reparti task = repartiRepository.findById(taskId)
+	                .orElseThrow(() -> new RuntimeException("Task not found"));
+	        if (task.getSpitali().getId() == employeeId) {
+	            repartiRepository.delete(task);
+	            return ResponseEntity.ok().body("Task with id " + taskId + " has been deleted");
+	        }
+	        throw new RuntimeException("Task not found");
+	    }
+
+	    public Reparti getTask(Long departmentId, Long employeeId, Long taskId) {
+	    	spitaliservice.getSpitali(departmentId, employeeId);
+	        Reparti task = repartiRepository.findById(taskId)
+	                .orElseThrow(() -> new RuntimeException("Task not found"));
+	        if (task.getSpitali().getId() == employeeId) {
+	            return task;
+	        }
+	        throw new RuntimeException("Task not found");
+	    }
+	    public Reparti findById(Long id) {return repartiRepository.findById(id).orElse(null);}
+	    
+	    public Reparti getTask1(Long departmentId) {
+	    	spitaliservice.getSpitali1(departmentId);
+	        Reparti task = repartiRepository.findById(departmentId)
+	                .orElseThrow(() -> new RuntimeException("Task not found"));
+	        if (task.getSpitali().getId() == departmentId) {
+	            return task;
+	        }
+	        throw new RuntimeException("Task not found");
+	    }
+	}
+
---
+++ menaxhimispitalit/src/main/java/org/meanxhimispitalit/menaxhimispitalit/service/SpitaliService.java
@@ -0,0 +1,91 @@
+package org.meanxhimispitalit.menaxhimispitalit.service;
+
+import org.meanxhimispitalit.menaxhimispitalit.Entity.Spitali;
+import org.meanxhimispitalit.menaxhimispitalit.Entity.Qyteti;
+
+import org.meanxhimispitalit.menaxhimispitalit.repository.SpitaliRepository;
+import org.meanxhimispitalit.menaxhimispitalit.repository.QytetiRepository;
+import org.meanxhimispitalit.menaxhimispitalit.service.QytetiService;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+
+
+import java.util.List;
+
+@Service
+
+public class SpitaliService {
+	
+
+	    private final SpitaliRepository spitalirepository;
+	    private final QytetiRepository qytetiRepository;
+	    private final QytetiService qytetiService;
+
+        public SpitaliService(SpitaliRepository spitalirepository, QytetiRepository qytetiRepository, QytetiService qytetiService) {
+            this.spitalirepository = spitalirepository;
+            this.qytetiRepository = qytetiRepository;
+            this.qytetiService = qytetiService;
+        }
+	    
+	   
+	    public Spitali teOrUpdateMjeket(Spitali spitalet,long departmentId)
+	    {
+	    	spitalet.setQyteti(qytetiService.findById(departmentId));
+	    	return spitalirepository.save(spitalet);
+	    	}
+	    
+
+	    public Spitali findById(long id) {return spitalirepository.findById(id).orElse(null);}
+	   
+	    
+	    public List<Spitali> getSpitaliList(long repartiId){	
+	    	return qytetiRepository.findById(repartiId).get().getSpitalet();
+	    	}
+	    
+	    public ResponseEntity<?> deleteMjeketById(long repartiId ,long id){Spitali spitalet = findById(id);
+	    if(spitalet != null){
+	    	if(spitalet.getQyteti().getId() == repartiId) {
+	    	
+	    		spitalirepository.delete(spitalet);
+	    String massage = "Employee with id:" + spitalet.getId()+ "has been deleted";
+	    return ResponseEntity.ok().body(massage);
+	    
+	    	}
+	    	else {String massage = "Employee was not found";
+	        throw new RuntimeException(massage);
+	    		
+	    	}
+	    }
+	    else{String massage = "Employee was not found";
+	         throw new RuntimeException(massage);
+	      }
+	    } 
+	 
+	    public Spitali getSpitali(Long repartiId,Long mjeketID) {
+	    	
+	    	Spitali spitalet = findById(mjeketID);
+	    	
+	    	  if(spitalet == null ||spitalet.getQyteti().getId() != repartiId ) {
+	    		  throw new RuntimeException("Employe not found ");
+	    	  }
+	    
+	    	return spitalet;
+	    }
+	    
+	    public Spitali getSpitali1(Long repartiId) {
+	    	
+	    	Spitali spitalet = findById(repartiId);
+	    	
+	    	  if(spitalet == null ||spitalet.getQyteti().getId() != repartiId ) {
+	    		  throw new RuntimeException("Employe not found ");
+	    	  }
+	    
+	    	return spitalet;
+	    }
+	}
+
+
+
