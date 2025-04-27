56e29e39be259161378f7fc5dcfcee47a278f1ad
Drilonnol <150484873+Drilonnol@users.noreply.github.com>
2025-04-27 22:15:52 +0200

    we create all of the component need to run and connect sucesses

---
+++ menaxhimispitalit/src/main/java/org/meanxhimispitalit/menaxhimispitalit/controller/MjeketController.java
@@ -0,0 +1,69 @@
+package org.meanxhimispitalit.menaxhimispitalit.controller;
+
+
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.meanxhimispitalit.menaxhimispitalit.Entity.Mjeket;
+import org.meanxhimispitalit.menaxhimispitalit.service.MjeketService;
+
+
+
+
+@RestController
+@RequestMapping("/api/mjeket")
+@CrossOrigin(origins = "http://localhost:3000")
+public class MjeketController {
+
+    private final MjeketService mjeketService;
+  
+    public MjeketController(MjeketService mjeketService) {
+        this.mjeketService = mjeketService;
+    }
+
+    @PostMapping(path = "/{qytetiId}/{SpitaliID}/{repartiID}")
+    public ResponseEntity<?> createMjeket(@PathVariable Long qytetiId, @PathVariable Long SpitaliID,
+                                          @PathVariable Long repartiID,
+                                        @RequestBody Mjeket mjeket
+                                         ) {
+        return ResponseEntity.ok(mjeketService.teOrUpdateMjeket(mjeket, qytetiId, SpitaliID, repartiID));
+    }
+
+    /* @PostMapping(path = "/{qytetiId}/{SpitaliID}/{repartiID}")
+     public ResponseEntity<?> createMjeket(@PathVariable Long qytetiId, @PathVariable Long SpitaliID,
+                                           @PathVariable Long repartiID,
+                                           @RequestBody Mjeket mjeket,
+                                           @RequestHeader("Authorization") String token) {
+
+        
+         ApiResponse isAdminResponse = adminService.checkAdmin(token);
+         if (!isAdminResponse.isStatus()) {
+             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("You are not authorized to create a doctor.");
+         }
+
+         return ResponseEntity.ok(mjeketService.teOrUpdateMjeket(mjeket, qytetiId, SpitaliID, repartiID));
+     }
+  */
+
+
+    @GetMapping(path = "/o/{qytetiId}/{spitaliId}/{repartiId}/{mjekuId}")
+    public Mjeket getEmployeeById(@PathVariable Long qytetiId, @PathVariable Long spitaliId,
+                                  @PathVariable Long repartiId, @PathVariable Long mjekuId) {
+        return mjeketService.getMjke(qytetiId, spitaliId, repartiId, mjekuId);
+    }
+
+    @GetMapping(path = "/all/{qytetiId}/{spitaliId}/{repartiId}")
+    public List<Mjeket> getEmployeeList(@PathVariable Long qytetiId, @PathVariable Long spitaliId, @PathVariable Long repartiId) {
+        return mjeketService.getMjeketList(qytetiId, spitaliId, repartiId);
+    }
+
+    @DeleteMapping(path = "/delete/{departmentId}/{id}")
+    public ResponseEntity<?> deleteEmployeeById(
+                                                @PathVariable Long departmentId, @PathVariable Long id) {
+      
+        return mjeketService.deleteMjeketById(departmentId, id);
+    }
+}
